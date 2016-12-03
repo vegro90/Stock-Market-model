5c5daf4a2708ca1e5358b84ed57b31870dff2e86
sindrerb <s.r.bilden@fys.uio.no>
2016-12-03 19:33:39 +0100

    Very rough scripts to visualize results

---
+++ python/eqHist.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 13:29:07 2016
+
+@author: sindrerb
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fileName = "../release/src/test_EqHist.txt"
+file = np.loadtxt(fileName,dtype = float, skiprows = 2)
+file = file[:]
+    
+plt.hist(file,10,alpha=0.3);
+plt.legend()
--- python/stockdistribution.py
+++ python/stockdistribution.py
@@ -1,34 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fileName = "../release/src/testHist.txt"
-file = np.loadtxt(fileName,dtype = float)
-numberOfAgents = int(file[0])
-step = file[1]
-file = file[2:]
-
-x = np.linspace(0,len(file)*step,len(file))
 fig = plt.figure(figsize=(15,10))
-fig.suptitle("Distribution with "+str(numberOfAgents)+" agents.",size=20)
+fig.suptitle("Distribution",size=20)
 
 ax1 = fig.add_subplot(1,1,1)
+
+hists = [0,10]
+
+step = 0.01
+
+fileName = "../release/src/runA_CapitalDistribution.txt"
+file = np.loadtxt(fileName,dtype = float,skiprows=7)
+
+x = np.linspace(0,len(file)*step,len(file))
 
 y1 = 0;
 y2 = file;
 
-file = file/np.trapz(file,x)
+file = file/np.trapz(file,dx=0.01)
 
 #ax1.fill_between(x, y1, y2, where=y2 >= y1, interpolate=True)    
 ax1.plot(x,file)
 x = x[60:]
 """
-for i in np.linspace(0.5,2,4):
+    for i in np.linspace(0.5,2,4):
     norm = np.trapz(x**(-1-i),x)
     ax1.plot(x,x**(-1-i)/norm,label='alpha=%f'%i)
-"""
+    """
 ax1.plot(x, np.exp(-x))
-ax1.set_ylim(1e-7,1e0)
-ax1.set_xlim(0,30)
+#ax1.set_ylim(1e-6,2.5e0)
+ax1.set_xlim(0,2)
 ax1.legend()
 #ax1.set_yscale('log')
 
